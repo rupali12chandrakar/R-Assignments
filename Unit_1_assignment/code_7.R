c24a5e9ce9caa8884d5e34efab8e346768317a9b
Rupali Chandrakar <148358159+rupali12chandrakar@users.noreply.github.com>
2025-01-30 22:38:47 +0530

    Create code_7.R

---
+++ Unit_1_assignment/code_7.R
@@ -0,0 +1,5 @@
+#Extract the first and last elements of your vector from (6.), storing them as a new object.
+print(vec6 <- c(seq(3, 6, length.out = 5)))
+print(rep(c(2, -5.1, -33), 2))
+print(7/42 + 2)
+print(first_last <- c(vec6[1], vec6[length(vec6)]))
