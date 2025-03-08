e61eef6c8afef4a667454078772d99bf1732f61c
Rupali Chandrakar <148358159+rupali12chandrakar@users.noreply.github.com>
2025-03-08 11:08:16 +0530

    Create sol_4.R

---
+++ Unit_3_Assignment/sol_4.R
@@ -0,0 +1,8 @@
+log_vector <- matrix(rep(c(TRUE, FALSE), times = 6), nrow = 3, ncol = 4)
+print(log_vector)
+      
+numeric_vector<-matrix(c(3,5,2,6,-4,8,-2,-1,4,2,-12,3),nrow=3,ncol=4)
+print(numeric_vector)
+     
+ comparison<-log_vector == (numeric_vector<0)
+ print(comparison)
