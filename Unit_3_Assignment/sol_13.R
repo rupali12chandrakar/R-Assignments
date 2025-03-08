8ad904f9d6b10a1f6cebb573d4c200eea9847ae2
Rupali Chandrakar <148358159+rupali12chandrakar@users.noreply.github.com>
2025-03-08 16:11:26 +0530

    Create sol_13.R

---
+++ Unit_3_Assignment/sol_13.R
@@ -0,0 +1,4 @@
+months <- factor(c("Jan", "Mar", "Feb", "Apr", "May", "Dec", "Nov"), 
+                 levels = c("Jan", "Feb", "Mar", "Apr", "May", "Nov", "Dec"), 
+                 ordered = TRUE)
+print(months)
